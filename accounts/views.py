d0f06fb5ac381043cfcfe9e537944982b37e1efe
Innocent Zenda <zendainnocent@gmail.com>
2020-09-28 13:44:50 +0300

    Add accounts app for handling users.

---
+++ accounts/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import CreateView
+
+from accounts import forms
+
+
+class UserRegistrationView(CreateView):
+    form_class = forms.UserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = '/'
--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'main.apps.MainConfig',
 
     'crispy_forms',
