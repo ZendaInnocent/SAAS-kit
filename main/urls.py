9adaec6b7df83c6e1496e0c164aaf8feb56ac931
Innocent Zenda <zendainnocent@gmail.com>
2020-10-02 12:55:40 +0300

    feature: add change plan functionality.

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index_view, name='home'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('change-plan/', views.change_plan, name='change-plan'),
+    path('confirm-plan/', views.confirm_plan, name='confirm-plan'),
+    path('thank-you/', views.thank_you, name='thank-you'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
 
 from .models import Plan
-from django.contrib.auth.decorators import login_required
 
 
 def index_view(request):
@@ -14,4 +17,63 @@ def index_view(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'main/dashboard.html')
+    context = {
+        'current_plan': request.user.subscription_set.first(),
+    }
+    return render(request, 'main/dashboard.html', context)
+
+
+@login_required
+def change_plan(request):
+    plans = Plan.objects.all()
+
+    if request.method == 'POST':
+        sub = request.user.subscription_set.first()
+        plan = request.POST['plan']
+        request.session['plan'] = plan
+        new_plan = get_object_or_404(Plan, name=plan)
+        if sub.plan == new_plan:
+            messages.info(
+                request, (f'You are current subscribed to {sub.plan} plan. '
+                          f'Please select different plan to change.'))
+            return redirect(reverse('main:change-plan'))
+        else:
+            context = {'new_plan': new_plan, 'current_plan': sub.plan}
+            return render(request, 'main/confirm_plan.html', context)
+
+    context = {
+        'plans': plans,
+    }
+    return render(request, 'main/change_plan.html', context)
+
+
+@login_required
+def confirm_plan(request):
+    if request.method == 'POST':
+        current_plan = request.user.subscription_set.first()
+        plan = request.session['plan'] if request.session['plan'] else None
+        if plan != current_plan.plan:
+            new_plan = get_object_or_404(Plan, name=plan)
+            # set new plan for the user
+            if request.user.is_authenticated:
+                user = request.user
+                # get user subscription
+                sub = user.subscription_set.first()
+                sub.plan = new_plan
+                sub.save()
+                del request.session['plan']
+                messages.success(
+                    request, 'You plan has changed successful.')
+            return render(request, 'main/thank_you.html', {
+                'new_plan': new_plan,
+            })
+
+    context = {
+        'current_plan': request.user.subscription_set.first(),
+    }
+    return render(request, 'main/confirm_plan.html', context)
+
+
+@login_required
+def thank_you(request):
+    return render(request, 'main/thank_you.html')
